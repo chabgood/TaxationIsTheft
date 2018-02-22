d0fd10eb8479ddef76e1ed711665298ed843a919
Chris Habgood <chabgood@gmail.com>
2018-02-22 17:13:26 -0600

    use NV bare

--- twit.rb
+++ twit.rb
@@ -1,9 +1,10 @@
 require 'twitter'
+p ENV['CONSUMER_KEY']
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "#{ENV['CONSUMER_KEY']}"
-  config.consumer_secret     = "#{ENV['CONSUMER_SECRET']}"
-  config.access_token        = "#{ENV['ACCESS_TOKEN']}"
-  config.access_token_secret = "#{ENV['ACCESS_TOKEN_SECRET']}"
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
 client.update("@realDonaldTrump
